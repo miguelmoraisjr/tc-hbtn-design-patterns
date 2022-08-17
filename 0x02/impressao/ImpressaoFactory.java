0e4371dbdca565b7cca416fecff1995e6678d013
miguelmoraisjr <miguelj@ciandt.com>
2022-08-17 12:05:19 -0300

    Factory Fabric

--- 0x02/impressao/ImpressaoFactory.java
+++ 0x02/impressao/ImpressaoFactory.java
@@ -1,16 +1,16 @@
 public class ImpressaoFactory {
 
-    public static Impressao createImpressao(TamanhoImpressao tamanhoImpressao, int totalPaginas,  int quantPaginasColoridas, boolean ehFrentreVerso) {
+    public static Impressao createImpressao(TamanhoImpressao tamanhoImpressao, int totalPaginas,  int quantPaginasColoridas, boolean ehFrenteVerso) {
         if (tamanhoImpressao == TamanhoImpressao.A2) {
-            Impressao impressao = new Impressao(totalPaginas, quantPaginasColoridas, ehFrentreVerso, 0.28,
+            Impressao impressao = new Impressao(totalPaginas, quantPaginasColoridas, ehFrenteVerso, 0.28,
                     0.18, 0.32, 0.22);
             return impressao;
         }  else if (tamanhoImpressao == TamanhoImpressao.A3) {
-            Impressao impressao = new Impressao(totalPaginas, quantPaginasColoridas, ehFrentreVerso, 0.25,
+            Impressao impressao = new Impressao(totalPaginas, quantPaginasColoridas, ehFrenteVerso, 0.25,
                     0.15, 0.30, 0.20);
             return impressao;
         } else {
-            Impressao impressao = new Impressao(totalPaginas, quantPaginasColoridas, ehFrentreVerso, 0.20,
+            Impressao impressao = new Impressao(totalPaginas, quantPaginasColoridas, ehFrenteVerso, 0.20,
                     0.10, 0.25, 0.15);
             return impressao;
         }
